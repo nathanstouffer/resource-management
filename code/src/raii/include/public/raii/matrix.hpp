57045a771fcb8548b717834eedbcd7cbe86eccf4
nathanstouffer <nathanstouffer1999@gmail.com>
2025-12-03 00:03:48 -0700

    Manage resources (#2)
    * add a destructor
    * use copy-and-swap for copy assignment
    * implement move constructor

--- code/src/raii/include/public/raii/matrix.hpp
+++ code/src/raii/include/public/raii/matrix.hpp
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 
+#include <utility>
+
 namespace raii
 {
 
@@ -9,12 +11,35 @@ namespace raii
     {
     public:
 
+        // constructor
         matrix(size_t m, size_t n);
+
+        // destructor
+        ~matrix();
+
+        // copy constructor
+        matrix(matrix const& rhs);
+
+        // move constructor
+        matrix(matrix&& rhs) noexcept;
+
+        // assignment operator (copied by value)
+        matrix& operator=(matrix rhs);
+
+        void swap(matrix& rhs) noexcept
+        {
+            using std::swap;
+            swap(m_rows, rhs.m_rows);
+            swap(m_cols, rhs.m_cols);
+            swap(m_values, rhs.m_values);
+        }
+
 
     private:
 
         size_t m_rows;
         size_t m_cols;
+        double* m_values;
 
     };
 
